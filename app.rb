dd66f231c35f73c1625367884dcb62b03462e3d5
louis baptiste <louisvis@ymail.com>
2019-03-07 07:01:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,15 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+  get '/newteam' do
+      erb :newteam
+  end
+
+    post '/team' do
+      original_string = params["string"]
+      @reversed_string = original_string.reverse
+
+      erb :reversed
+    end
+
 end
